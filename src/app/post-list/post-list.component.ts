259e52afab1c6a489ba95ff39f6a0150d9456a4f
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-03 20:57:27 -0300

    [Added] post-list

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PostDto } from 'src/models/post.dto';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit {
+  posts: PostDto[] = [
+    {
+      title: 'Mock',
+      description: 'This is a mock description',
+      bookmark_count: 300000,
+      tags: ['post', 'mock', 'tag'],
+    },
+    {
+      title: 'Mock 2',
+      description: 'This is a mock description 2',
+      bookmark_count: 1,
+      tags: [],
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
