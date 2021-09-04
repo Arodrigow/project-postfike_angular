248d6d2900704522f62fd8016fe9683791d7a1b2
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-04 11:31:07 -0300

    [Added] Biding custom properties

--- src/app/post-list/post/post.component.ts
+++ src/app/post-list/post/post.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PostDto } from 'src/models/post.dto';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  @Input() postElement!: PostDto;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
