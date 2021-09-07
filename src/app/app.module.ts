8eb2d15ccfd606cc762b1fee593402fc273c7514
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-07 10:03:15 -0300

    [Added] Getting main page posts from backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +20,7 @@ import { PostComponent } from './post-list/post/post.component';
   imports: [
     BrowserModule,
     NoopAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatGridListModule,
   ],
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { PostDto } from 'src/models/post.dto';
 
 @Component({
@@ -9,95 +9,24 @@ import { PostDto } from 'src/models/post.dto';
 })
 export class PostListComponent implements OnInit {
   breakpoint = 3;
-  posts: PostDto[] = [
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock 2',
-      description: 'This is a mock description 2',
-      bookmark_count: 1,
-      tags: [],
-      deadline: '30',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-    {
-      title: 'Mock',
-      description:
-        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
-      bookmark_count: 300000,
-      tags: ['post', 'mock', 'tag', 'outra', 'pra', 'testar'],
-      deadline: '3',
-    },
-  ];
+  posts: PostDto[] = []
 
-  constructor() {}
+  constructor(private http: HttpClient,) {}
 
   ngOnInit(): void {
+    this.getAllPosts()
     this.breakpoint = (window.visualViewport.width <= 400) ? 1 : 3;
   }
 
   onResize(event: any) {
     this.breakpoint = (event.target.visualViewport.width <= 400) ? 1 : 3;
+  }
+
+  getAllPosts() {
+    this.http.get<PostDto[]>('http://localhost:3000/posts').subscribe(
+      response => {
+        this.posts = response;
+      },
+      error => {});
   }
 }
