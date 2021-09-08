874ba8b9d39a028837957b480b6fe7c67caac413
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-08 12:30:34 -0300

    [Update] Post to user tag and user

--- src/models/post.dto.ts
+++ src/models/post.dto.ts
@@ -1,8 +1,15 @@
+import { UserDto } from './user.dto';
+import { TagDto } from './tag.dto';
 export interface PostDto {
+  id: string;
   title: string;
   description: string;
-  bookmark_count: number;
-  tags?: string[];
-  created_at?: Date;
-  deadline?: string;
+  details?: string;
+  created_at: Date;
+  updated_at: Date;
+  expiration: Date;
+  count_bookmark: number;
+
+  tags?: TagDto[];
+  user: UserDto;
 }
