ff21dea9ecd06ecfe9b5b1a426180bd1e7993137
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-14 11:18:44 -0300

    [Updated] Entrar and Sair buttons now working properly

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
   }
 
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  isLoggedOut() {
+    return this.authService.isLoggedOut();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
