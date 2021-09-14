c6f2ae54fb174b536e4d7e6e59666b12f2135402
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-09-14 11:17:59 -0300

    [Update] Auth service is dealing with storage, for now

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,6 +2,8 @@ import { CredentialsDto } from './../models/credentials.dto';
 import { API_CONFIG } from './../config/api.config';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import * as moment from 'moment';
+
 
 @Injectable()
 export class AuthService {
@@ -14,5 +16,37 @@ export class AuthService {
 
   login(credentials: CredentialsDto) {
     return this.http.post(`${API_CONFIG.baseUrl}/auth/login`, credentials, this.httpOptions)
+      .subscribe(response => {
+        this.setSession(response);
+        return true;
+      }, error => {})
   }
+
+  private setSession(authResult: any) {
+    const expiresIn = moment().add(authResult.expiresIn, 'seconds');
+
+    localStorage.setItem('id_token', authResult.access_token)
+    localStorage.setItem("expires_at", JSON.stringify(expiresIn.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration(): moment.MomentInput {
+    const expiration = localStorage.getItem("expires_at") as string;
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+
+
 }
